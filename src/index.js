588c6187240beb0f7f734711b83d9c295940f406
Dorkiee <ma19kdo@gmail.com>
2022-06-05 13:04:17 +0100

    Update Coming Soon

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); //const to pass "reactdom.createroot" at a certain element - "root"
 root.render( //render the content passed inside of the app component
+//UI Design here
     <App />
 );
 
